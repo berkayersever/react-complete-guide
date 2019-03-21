00ddc9d78a70493a4e85b944cb26502e3dd49e1f
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-21 23:08:04 +0300

    Used props inside the WrappedComponent with the spread operator.

--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const withClass = (WrappedComponent, className) => {
+const withClass = (WrappedComponent, className) => {    // {...props} Spread Operator
     return props => (
         <div className={className}>
-            <WrappedComponent/>
+            <WrappedComponent {...props}/>
         </div>
     );
 };
