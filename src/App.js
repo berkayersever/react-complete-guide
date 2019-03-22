86bb193b929a0d60813f51831737baf46e65260e
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-22 17:55:18 +0300

    Minor change.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+            <h1>React App</h1>
+            <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
