a75bad9b96bb52d1f8ab6981bd9cebe4547853cb
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-22 21:44:37 +0300

    Added a call to the switchNameHandler method when the button is clicked.

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ class App extends Component {
 
     switchNameHandler = () => {
         console.log('Was clicked!');
-    }
+    };
 
     render() {
         return (
             <div className="App">
                 <h1>React App</h1>
                 <p>This is really working!</p>
-                <button onClick={}>Switch Name</button>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
