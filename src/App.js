a4e46b59bde71777f9c55b8375769f9f37f0c980
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-22 21:07:23 +0300

    Started using the state property.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <h1>React App</h1>
-          <p>This is really working!</p>
-          <button>Switch Name</button>
-          <Person name="Max" age="28"/>
-          <Person name="Manu" age="29">My Hobbies: Racing</Person>
-          <Person name="Stephanie" age="26"/>
-      </div>
-    );
-    //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'React App'));
-  }
+    state = {
+        persons: [
+            { name: 'Max', age: 28 },
+            { name: 'Manu', age: 29 },
+            { name: 'Stephanie', age: 26}
+        ]
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>React App</h1>
+                <p>This is really working!</p>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div>
+        );
+        //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'React App'));
+    }
 }
 
 export default App;
