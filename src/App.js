1cf8cd83444b5ea264014d7f45907b027c354d4a
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-22 18:09:27 +0300

    Rendered the same thing using a different method.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-            <h1>React App</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //         <h1>React App</h1>
+    //   </div>
+    // );
+      return React.createElement('div', null, React.createElement('h1', null, 'React App'));
   }
 }
 
