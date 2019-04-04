d51d3e4bc37db7f7bc1ee0a8cb5e3c0aefeadebc
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-04 14:10:07 +0300

    Used an alternative way for copying the array.

--- src/App.js
+++ src/App.js
@@ -24,7 +24,8 @@ class App extends Component {
     }
 
     deletePersonHandler = (personIndex) => {
-        const persons = this.state.persons.slice();     // For copying the array
+        // const persons = this.state.persons.slice();  // For copying the array
+        const persons = [...this.state.persons];        // For copying the array
         persons.splice(personIndex, 1);
         this.setState({persons: persons});
     }
