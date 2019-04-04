dd2b0eeb8b4d69f5c35f66676bed501d615872b6
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-04 04:13:41 +0300

    Deleted switchNameHandler method.

--- src/App.js
+++ src/App.js
@@ -13,16 +13,6 @@ class App extends Component {
         showPersons: false
     }
 
-    switchNameHandler = (newName) => {
-        this.setState( {
-            persons: [
-                { name: newName, age: 28 },
-                { name: 'Manu', age: 29 },
-                { name: 'Stephanie', age: 27 }
-            ]
-        } )
-    }
-
     nameChangedHandler = (event) => {
         this.setState( {
             persons: [
@@ -52,17 +42,12 @@ class App extends Component {
         if(this.state.showPersons) {
             persons = (
                 <div>
-                    <Person
-                        name={this.state.persons[0].name}
-                        age={this.state.persons[0].age}/>
-                    <Person
-                        name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}
-                        click={this.switchNameHandler.bind(this, 'Max!')}
-                        changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-                    <Person
-                        name={this.state.persons[2].name}
-                        age={this.state.persons[2].age} />
+                    {this.state.persons.map(person => {
+                        return <Person
+                            name={person.name}
+                            age={person.age} />
+                    })}
+
                 </div>
             );
         }
