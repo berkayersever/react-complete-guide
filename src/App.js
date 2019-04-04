68b9019d03e577acb5ff2de50a11c59ff5fdccb3
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-04 03:42:01 +0300

    Refactored the code.

--- src/App.js
+++ src/App.js
@@ -72,8 +72,7 @@ class App extends Component {
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working!</p>
                 <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-
-                }
+                {persons}
             </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
