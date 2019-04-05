dcca490d3921dd1398907c80e15f2e3e0a1a5879
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-05 13:41:44 +0300

    Using CSS Modules without using 'npm run eject'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import styles from './App.module.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -75,14 +75,14 @@ class App extends Component {
         // let classes = ['red', 'bold'].join(' ');
         const classes = [];
         if (this.state.persons.length < 3) {
-            classes.push('red');    // classes = ['red']
+            classes.push(styles.red);    // classes = ['red']
         }
         if (this.state.persons.length < 2) {
-            classes.push('bold');   // classes = ['red', 'bold']
+            classes.push(styles.bold);   // classes = ['red', 'bold']
         }
 
         return (
-                <div className="App">
+                <div className={styles.App}>
                     <h1>Hi, I'm a React App</h1>
                     <p className={classes.join(' ')}>This is really working!</p>
                     <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
