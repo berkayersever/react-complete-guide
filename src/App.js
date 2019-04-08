a4251fe3f5ec6cfee4df5811349a3be8f1b9df6b
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-08 23:26:36 +0300

    Removed the ErrorBoundary.

--- src/App.js
+++ src/App.js
@@ -51,14 +51,12 @@ class App extends Component {
             persons = (
                 <div>
                     {this.state.persons.map((person, index) => {
-                        return <ErrorBoundary key={person.id}>
-                            <Person
-                                click={() => this.deletePersonHandler(index)}
-                                name={person.name}
-                                age={person.age}
-                                key={person.id}
-                                changed={(event) => this.nameChangedHandler(event, person.id)}/>
-                        </ErrorBoundary>
+                        return <Person
+                            click={() => this.deletePersonHandler(index)}
+                            name={person.name}
+                            age={person.age}
+                            key={person.id}
+                            changed={(event) => this.nameChangedHandler(event, person.id)}/>
                     })}
                 </div>
             );
