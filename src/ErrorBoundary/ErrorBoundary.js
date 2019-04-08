362c4f1ffdba8711c14e99dcdfeb0398796744e6
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-08 21:48:08 +0300

    Created state property.

--- src/ErrorBoundary/ErrorBoundary.js
+++ src/ErrorBoundary/ErrorBoundary.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 
 class ErrorBoundary extends Component {
-
+    state = {
+        hasError: false,
+        errorMessage: ''
+    }
 
     render() {
         return <h1>Oops! Something went wrong.</h1>;
