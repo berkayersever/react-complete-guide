ee9f7367ae03f85f552d884a6e1a013a97d9ae34
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-09 23:58:20 +0300

    Implemented the Cockpit.js

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const cockpit = () => {
-    return ();
+    return (
+        <div>
+            <h1>Hi, I'm a React App</h1>
+            <p className={classes.join(' ')}>This is really working!</p>
+            <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        </div>
+    );
 };
 
 export default cockpit;
