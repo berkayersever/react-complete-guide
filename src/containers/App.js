186d08448a237c4f5ac6a663c8b5c66649a69e54
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-12 20:13:53 +0300

    Minor changes.

--- src/containers/App.js
+++ src/containers/App.js
@@ -64,7 +64,7 @@ class App extends Component {
     }
 
     render() {
-        console.log('[App.js] render')
+        console.log('[App.js] is rendering...')
         let persons = null;
         if (this.state.showPersons) {
             persons = <Persons
