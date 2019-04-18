e48c3f705a36c024f20c5349a87bdc446048aaf5
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-18 22:51:04 +0300

    Added a return statement inside the useEffect method.

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -8,6 +8,9 @@ const cockpit = (props) => {
         setTimeout(() => {
             alert('Saved data to cloud!');
         }, 1000);
+        return () => {
+            console.log('[Cockpit.js] cleanup work in useEffect');
+        };
     }, []);
 
     const classes = [];
