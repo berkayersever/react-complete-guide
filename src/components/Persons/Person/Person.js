493630743eed3ac2e185d18a43193ab18a1946af
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-20 22:36:10 +0300

    Minor change.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Aux from '../../../hoc/Auxiliary';
 import styles from './Person.module.css';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] is rendering...');
-        return [
-                <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
-                <p key="i2">{this.props.children}</p>,
-                <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
-        ];
+        return (
+        <Aux>
+            <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+            <p key="i2">{this.props.children}</p>
+            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
+        </Aux>
+        );
     }
 }
 
