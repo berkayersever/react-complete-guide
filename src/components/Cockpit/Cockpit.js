f12f707a2d06fed63e733a8fc7f8be0207f4cf2f
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-22 23:02:09 +0300

    Added onClick to the button.

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -41,7 +41,7 @@ const cockpit = (props) => {
             <h1>{props.title}</h1>
             <p className={classes.join(' ')}>This is really working!</p>
             <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>Toggle Persons</button>
-            <button></button>
+            <button onClick={props.login}>Login</button>
         </div>
     );
 };
