c7346a6fca21343f438a9e83922afc2f468a6d23
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-22 22:05:13 +0300

    Refactored the render method.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -15,7 +15,12 @@ class Person extends Component {
         <Aux>
             <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
             <p key="i2">{this.props.children}</p>
-            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
+            <input
+                key="i3"
+                type="text"
+                onChange={this.props.changed}
+                value={this.props.name}
+            />
         </Aux>
         );
     }
