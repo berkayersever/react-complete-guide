c096f44703db1b5d263b9493483befcfce3b573b
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-22 22:13:18 +0300

    Minor change.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -10,7 +10,8 @@ class Person extends Component {
     }
 
     componentDidMount() {
-        this.inputElement.focus();
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
     }
 
     render() {
