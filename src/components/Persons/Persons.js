42e32a234daf074d67d4c8f21b496805603491c6
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-05-02 22:01:56 +0300

    Minor change.

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -49,7 +49,7 @@ class Persons extends PureComponent {
                 age={person.age}
                 key={person.id}
                 changed={(event) => this.props.changed(event, person.id)}
-                isAuth={props.isAuthenticated} />
+                isAuth={this.props.isAuthenticated} />
         });
     }
 }
